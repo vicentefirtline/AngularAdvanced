17b484688c2f45991ea9f5bddd1ab5272feaa260
vicentefirtline <vicentecoimbra030@gmail.com>
2026-05-18 11:19:31 -0300

    implementacao calculadora logica via angular

--- basic-concepts/src/app/calculadora/calculadora.ts
+++ basic-concepts/src/app/calculadora/calculadora.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculadora',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './calculadora.html',
   styleUrl: './calculadora.scss',
 })
-export class Calculadora {}
+export class Calculadora {
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+
+  calcularResultado() {
+    this.resultado = this.numero1 + this.numero2;
+    console.log('Calculando resultado...'+this.resultado);
+    console.log('Número 1:', this.numero1);
+    console.log('Número 2:', this.numero2); 
+  }
+}
