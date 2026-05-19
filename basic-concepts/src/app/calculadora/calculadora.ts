4d91f68e53a629508254baf2a398423549a5066d
Vicente Coimbra <vicentecoimbra030@gmail.com>
2026-05-19 15:56:17 -0300

    Atualizando o projeto Angular

--- basic-concepts/src/app/calculadora/calculadora.ts
+++ basic-concepts/src/app/calculadora/calculadora.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculadora',
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './calculadora.html',
   styleUrl: './calculadora.scss',
 })
